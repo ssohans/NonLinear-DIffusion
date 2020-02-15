201f6dc8c3dd370a4284cf2dc84576ae0cb41cd9
Sayed Sohan <15130913+goldenbijoy@users.noreply.github.com>
2020-02-15 17:10:59 +0600

    Update Anisotropic_diffission.py

--- Anisotropic_diffission.py
+++ Anisotropic_diffission.py
@@ -9,6 +9,30 @@ import numpy as np
 from matplotlib import pyplot as plt
 import cv2
 
+def north(img):
+   north = np.zeros_like(img)
+   north[1:,:] = img[:-1,:]
+   north[0,:] = img[0,:]
+   return north - img
+
+def south(img):
+   south = np.zeros_like(img)
+   south[:-1,:] = img[1:,:]
+   south[-1,:] = img[-1,:]
+   return south - img
+
+def west(img):
+   west = np.zeros_like(img)
+   west[:,1:] = img[:,:-1]
+   west[:,0] = img[:,0]
+   return west - img
+
+def east(img):
+   east = np.zeros_like(img)
+   east[:,:-1] = img[:,1:]
+   east[:,-1] = img[:,-1]
+   return east - img
+
 def func(s,k,eq=1):
    if eq==1:
       return 1./(1.+(s/k)**2)
@@ -18,29 +42,14 @@ def Anisotropic(img,it=50,k=40,gamma=.25):
    
    for i in range(it):
       #north gradient
-      north = np.zeros_like(img)
-      north[1:,:] = img[:-1,:]
-      north[0,:] = img[0,:]
-      dn = north - img
-      
+      dn = north(img)
       #south gradient
-      south = np.zeros_like(img)
-      south[:-1,:] = img[1:,:]
-      south[-1,:] = img[-1,:]
-      ds = south - img
-      
+      ds = south(img)   
       #West gradient
-      west = np.zeros_like(img)
-      west[:,1:] = img[:,:-1]
-      west[:,0] = img[:,0]
-      dw = west - img
-      
+      dw = west(img)
       #East gradient
-      east = np.zeros_like(img)
-      east[:,:-1] = img[:,1:]
-      east[:,-1] = img[:,-1]
-      de = east - img
-      
+      de = east(img)
+          
       n = func(dn,k)
       s = func(ds,k)
       w = func(dw,k)
@@ -62,13 +71,13 @@ def noise(img):
    return noisy_image 
    
 
-img = cv2.imread('img/neuron.jpg',0)
+img = cv2.imread('img/brain.PNG',0)
 
 img = noise(img)
 
 img1 = img.astype('float32')/255
 
-img1  = Anisotropic(img1,35)
+img1  = Anisotropic(img1,1)
 
 plt.figure(figsize=(10,12))
 plt.subplot(121).imshow(img,cmap='gray')
